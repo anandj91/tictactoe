fe89b5b0602f3195a9fe9b9e30ebb8151df9b930
anandj91 <anand.indukala@gmail.com>
2014-10-19 12:47:47 +0530

    Basic functionalities 	1. setting text 'x' and 'o' alternatively while clicking a button

--- src/com/example/tictactoe/MainActivity.java
+++ src/com/example/tictactoe/MainActivity.java
@@ -2,14 +2,96 @@ package com.example.tictactoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	boolean checkCROSS[] = new boolean[9];
+	boolean checkDOT[] = new boolean[9];
+
+	enum TURN {
+		CROSS, DOT
+	}
+
+	TURN turn = TURN.CROSS;
+
+	private void setCellImage(Button b) {
+		if(b.getText() == "X" || b.getText() == "O")
+			return;
+		
+		if (turn == TURN.CROSS) {
+			turn = TURN.DOT;
+			b.setText("X");
+			checkCROSS[getIndex(b.getId())] = true;
+		} else {
+			turn = TURN.CROSS;
+			b.setText("O");
+			checkDOT[getIndex(b.getId())] = true;
+		}
+	}
+	
+	private boolean checkGame(){
+		
+		
+		return false;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		Button[] cells = { (Button) findViewById(R.id.cell1),
+				(Button) findViewById(R.id.cell2),
+				(Button) findViewById(R.id.cell3),
+				(Button) findViewById(R.id.cell4),
+				(Button) findViewById(R.id.cell5),
+				(Button) findViewById(R.id.cell6),
+				(Button) findViewById(R.id.cell7),
+				(Button) findViewById(R.id.cell8),
+				(Button) findViewById(R.id.cell9) };
+
+		for (int i = 0; i < 9; i++) {
+			Log.i("TIC-TAC-TOE", i + "");
+			cells[i].setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					setCellImage((Button) v);
+				}
+			});
+		}
+
+	}
+
+	private int getIndex(int id) {
+		switch (id) {
+		case R.id.cell1:
+			return 0;
+		case R.id.cell2:
+			return 1;
+		case R.id.cell3:
+			return 2;
+		case R.id.cell4:
+			return 3;
+		case R.id.cell5:
+			return 4;
+		case R.id.cell6:
+			return 5;
+		case R.id.cell7:
+			return 6;
+		case R.id.cell8:
+			return 7;
+		case R.id.cell9:
+			return 8;
+		default:
+			return -1;
+
+		}
 	}
 
 }
